ca74ceace645e14863a793371755655b568d98d3
Natasha  Brand <100390018+Schoenertag@users.noreply.github.com>
2022-06-03 13:49:35 +0000

    quite literally everything, a lot went wrong just then -> music player functionality + styling, timer styling, sidebar collapse function

--- common.js
+++ common.js
@@ -1,15 +1,18 @@
-const sidebar = document.getElementById("sidebarDisplay");
+//collapse and reopen sidebar navigation 
 const sidebarMenuButton = document.getElementById("menuBtn");
+const sidebar = document.getElementById("sidebarDisplay");
 
 
 sidebarMenuButton.addEventListener("click", () => {
-//check if side bar is open
-let sidebarOpen = sidebar.classList.contains("toggleOn");
- if (sidebarOpen) { //if open, close it
+  //check if side bar is open
+  let sidebarOpen = sidebar.classList.contains("toggleOn");
+  if (sidebarOpen) {
+    //if open, close it
     sidebar.classList.remove("toggleOn");
   }
-  else { //if closed, open it
+  else {
+    //if closed, open it
     sidebar.classList.add("toggleOn");
   }
 
-});
+})
