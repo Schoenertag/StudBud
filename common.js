1b41cde5ea3377ced54e09849965f50750006e2b
Natasha  Brand <100390018+Schoenertag@users.noreply.github.com>
2022-06-03 12:51:24 +0000

    added media query for mobile functionality

---
+++ common.js
@@ -0,0 +1,21 @@
+//collapse and reopen sidebar navigation 
+const sidebarMenuButton = document.getElementById("menuBtn");
+const sidebar = document.getElementById("sidebarDisplay");
+
+
+sidebarMenuButton.addEventListener("click", () => {
+  //check if side bar is open
+  let sidebarOpen = sidebar.classList.contains("toggleOn");
+  if (sidebarOpen) {
+    //if open, close it
+    sidebar.classList.remove("toggleOn");
+  }
+  else {
+    //if closed, open it
+    sidebar.classList.add("toggleOn");
+  }
+
+});
+
+
+
